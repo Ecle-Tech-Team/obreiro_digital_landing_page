5e06f906e5a0e9b06a979a4bab114a4ee4b32687
MathewsSmicaluk <mathews.git@gmail.com>
2025-09-26 16:21:43 -0300

    criacao tela 404

---
+++ src/app/production/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Production() {
+  return (
+    <main>
+      <section>
+        <div>
+          <h1>PAGINA</h1>
+          <h1>EM PRODUÇÃO</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
